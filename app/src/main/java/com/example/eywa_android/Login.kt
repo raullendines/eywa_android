86461ea1107aa46c76e0c658d64b118ac3f725dc
Raul Lendines <91599628+raullendines@users.noreply.github.com>
2022-10-19 11:13:59 +0200

    Login with features

--- app/src/main/java/com/example/eywa_android/Login.kt
+++ app/src/main/java/com/example/eywa_android/Login.kt
@@ -1,11 +1,44 @@
 package com.example.eywa_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
 import android.os.Bundle
+import android.text.InputType
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val img_on = findViewById<ImageView>(R.id.show_pass_btn)
+        val img_off = findViewById<ImageView>(R.id.hide_pass_btn)
+        val password = findViewById<EditText>(R.id.edit_password)
+
+        //Turn on password
+        img_on.setOnClickListener {
+            if (img_on.isVisible){
+                img_on.visibility =View.INVISIBLE
+                img_off.visibility =View.VISIBLE
+                password.setInputType(InputType.TYPE_CLASS_TEXT);
+                println("on")
+            }
+        }
+
+        //Turn off password
+        img_off.setOnClickListener {
+            if (img_off.isVisible){
+                img_on.visibility =View.VISIBLE
+                img_off.visibility =View.INVISIBLE
+                password.setInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)
+                println("off")
+            }
+        }
+
+
     }
 }
