f55f2a80d9ca1287ca202cd5ab929cb45b80ac0f
MarcelHomar <99321235+MarcelHomar@users.noreply.github.com>
2022-10-20 10:44:06 +0200

    main page done

---
+++ app/src/main/java/com/example/eywa_android/Home.kt
@@ -0,0 +1,19 @@
+package com.example.eywa_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import com.example.eywa_android.R
+
+class Home : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
