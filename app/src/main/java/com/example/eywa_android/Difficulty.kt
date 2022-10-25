6920d1228e5ba27a362d602aeebeba430bb6953a
MarcelHomar <99321235+MarcelHomar@users.noreply.github.com>
2022-10-25 13:06:39 +0200

    fab lang menu display
    Function of display the lang menu and disable other views. Also change the lang and the resources

--- app/src/main/java/com/example/eywa_android/Difficulty.kt
+++ app/src/main/java/com/example/eywa_android/Difficulty.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.transition.Transition
 import android.transition.TransitionManager
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -15,53 +16,103 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.transition.MaterialContainerTransform
 
 class Difficulty : AppCompatActivity() {
+
+    var difficultySelected : Int = 0
+    var lang : String = "eng"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_difficulty)
 
+
         val fabMenu = findViewById<FloatingActionButton>(R.id.fab)
         //val close = findViewById<TextView>(R.id.tvClose)
         val linearLay = findViewById<LinearLayout>(R.id.LinLayout)
-        var menuExpanded : Boolean = false
+
+        val btnEasy = findViewById<Button>(R.id.btnEasy)
+        val btnMedium = findViewById<Button>(R.id.btnMedium)
+        val btnHard = findViewById<Button>(R.id.btnHard)
+        val btnLegend = findViewById<Button>(R.id.btnLegend)
+
+        val difficultyBtn = arrayOf<Button>(
+            btnEasy, btnMedium, btnHard, btnLegend
+        )
+
+        btnEasy.setOnClickListener(){
+            if (difficultySelected != 0){
+                difficultyBtn[difficultySelected].setBackgroundResource(R.drawable.rounded_corners_white)
+                difficultySelected = 0
+                btnEasy.setBackgroundResource(R.drawable.rounded_corners_yellow)
+            }
+        }
+
+        btnMedium.setOnClickListener(){
+            if (difficultySelected != 1){
+                difficultyBtn[difficultySelected].setBackgroundResource(R.drawable.rounded_corners_white)
+                difficultySelected = 1
+                btnMedium.setBackgroundResource(R.drawable.rounded_corners_yellow)
+            }
+        }
+
+        btnHard.setOnClickListener(){
+            if (difficultySelected != 2){
+                difficultyBtn[difficultySelected].setBackgroundResource(R.drawable.rounded_corners_white)
+                difficultySelected = 2
+                btnHard.setBackgroundResource(R.drawable.rounded_corners_yellow)
+            }
+        }
+
+        btnLegend.setOnClickListener(){
+            if (difficultySelected != 3){
+                difficultyBtn[difficultySelected].setBackgroundResource(R.drawable.rounded_corners_white)
+                difficultySelected = 3
+                btnLegend.setBackgroundResource(R.drawable.rounded_corners_yellow)
+            }
+        }
+
+
 
 
         fabMenu.setOnClickListener(){
             //fabMenu.setExpanded(true)
-            cositas(fabMenu)
+            displayLangMenu(fabMenu)
         }
 
         /*close.setOnClickListener(){
             fabMenu.setExpanded(false)
         }
         */
-        val txt1 = findViewById<TextView>(R.id.txtView1)
-        val txt2 = findViewById<TextView>(R.id.txtView2)
-        val txt3 = findViewById<TextView>(R.id.txtView3)
+        val catalanLayout = findViewById<LinearLayout>(R.id.catalanLayout)
+        val espanyolLayout = findViewById<LinearLayout>(R.id.espanyolLayout)
+        val englishLayout = findViewById<LinearLayout>(R.id.englishLayout)
 
-        txt1.setOnClickListener(){
-            cositas2(fabMenu)
-            fabMenu.background = resources.getDrawable(R.drawable.catalunya_bandera, theme)
+        catalanLayout.setOnClickListener(){
+            hideLangMenu(fabMenu)
+            fabMenu.setImageResource(R.drawable.catalunya_bandera)
+            lang = "cat"
+            changeLang(difficultyBtn)
+        }
+        espanyolLayout.setOnClickListener(){
+            hideLangMenu(fabMenu)
+            fabMenu.setImageResource(R.drawable.espanya_bandera)
+            lang = "esp"
+            changeLang(difficultyBtn)
+        }
+        englishLayout.setOnClickListener(){
+            hideLangMenu(fabMenu)
+            fabMenu.setImageResource(R.drawable.english_bandera)
+            lang = "eng"
+            changeLang(difficultyBtn)
+        }
 
-        }
-        txt2.setOnClickListener(){
-            cositas2(fabMenu)
-
-        }
-        txt3.setOnClickListener(){
-            cositas2(fabMenu)
-        }
-        when(menuExpanded){
-            true -> {
-
-            }
-        }
 
 
     }
 
-    private fun cositas(fabMenu : FloatingActionButton){
+    private fun displayLangMenu(fabMenu : FloatingActionButton){
         val endCard = findViewById<MaterialCardView>(R.id.end_card)
         val myLayout = findViewById<LinearLayout>(R.id.LinLayout)
+        val backgroundOpacity = findViewById<LinearLayout>(R.id.backgroundOpacity)
         val root = findViewById<CoordinatorLayout>(R.id.root)
         val goBack = findViewById<TextView>(R.id.goBack)
         val transition : MaterialContainerTransform = MaterialContainerTransform()
@@ -70,7 +121,7 @@ class Difficulty : AppCompatActivity() {
 
         transition.setInterpolator(myInterpolator)
 
-        transition.setDuration(1)
+        transition.setDuration(0)
 
         transition.startView = fabMenu
         transition.endView = endCard
@@ -85,20 +136,22 @@ class Difficulty : AppCompatActivity() {
             endCard.visibility = View.VISIBLE
         }
 
-        myLayout.setBackgroundColor(Color.parseColor("#40000000"))
+
+        backgroundOpacity.visibility = View.VISIBLE
         goBack.isClickable = true
         goBack.visibility = View.VISIBLE
         goBack.setOnClickListener(){
-            cositas2(fabMenu)
+            hideLangMenu(fabMenu)
 
         }
 
     }
 
-    private fun cositas2(fabMenu : FloatingActionButton){
+    private fun hideLangMenu(fabMenu : FloatingActionButton){
         val goBack = findViewById<TextView>(R.id.goBack)
         goBack.isClickable = false
         goBack.visibility = View.INVISIBLE
+        val backgroundOpacity = findViewById<LinearLayout>(R.id.backgroundOpacity)
         val startCard = findViewById<MaterialCardView>(R.id.end_card)
         val myLayout = findViewById<LinearLayout>(R.id.LinLayout)
         val root = findViewById<CoordinatorLayout>(R.id.root)
@@ -118,9 +171,41 @@ class Difficulty : AppCompatActivity() {
         if(fabMenu != null){
             fabMenu.visibility = View.VISIBLE
         }
-        myLayout.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        backgroundOpacity.visibility = View.INVISIBLE
+    }
+
+    private fun changeLang(difficultyBtn : Array<Button>){
+        val txtViewDifficulty = findViewById<TextView>(R.id.txtViewDifficulty)
+        val playButton = findViewById<Button>(R.id.btnPlay)
+        when(lang){
+            "cat" -> {
+                txtViewDifficulty.setText("DIFICULTAT")
+                difficultyBtn[0].setText("FÀCIL")
+                difficultyBtn[1].setText("MITJANA")
+                difficultyBtn[2].setText("DIFICIL")
+                difficultyBtn[3].setText("LLEGENDA")
+                playButton.setText("JUGAR")
+            }
+            "esp" -> {
+                txtViewDifficulty.setText("DIFICULTAD")
+                difficultyBtn[0].setText("FÁCIL")
+                difficultyBtn[1].setText("MEDIANA")
+                difficultyBtn[2].setText("DIFÍCIL")
+                difficultyBtn[3].setText("LEYENDA")
+                playButton.setText("JUGAR")
+            }
+            "eng" -> {
+                txtViewDifficulty.setText("DIFFICULTY")
+                difficultyBtn[0].setText("EASY")
+                difficultyBtn[1].setText("MEDIUM")
+                difficultyBtn[2].setText("HARD")
+                difficultyBtn[3].setText("LEGEND")
+                playButton.setText("PLAY")
+            }
+        }
+
     }
 
 
-}
+
 }
