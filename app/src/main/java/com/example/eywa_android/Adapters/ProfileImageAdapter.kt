ab8065e81f8d6847478591091b61ed6a818609be
MarcelHomar <99321235+MarcelHomar@users.noreply.github.com>
2022-11-15 13:56:04 +0100

    list adapter 1

---
+++ app/src/main/java/com/example/eywa_android/Adapters/ProfileImageAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.eywa_android.Adapters
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.eywa_android.R
+
+class ProfileImageAdapter(private val context: Context,
+                          private val images: MutableList<String>) :
+    RecyclerView.Adapter<ProfileImageAdapter.ImageViewHolder>(),
+    View.OnClickListener{
+
+    private val layout = R.layout.image_item
+
+    private var clickListener : View.OnClickListener? = null
+
+
+    class ImageViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+        var imageProfile : ImageView
+
+        init {
+            imageProfile = view.findViewById(R.id.imageToDisplay)
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
+        view.setOnClickListener(this)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val image = images[position]
+        bindImage(holder, image)
+
+    }
+
+    fun bindImage(holder: ImageViewHolder, image: String){
+        val imagePath = context.filesDir.toString() + "/img/" + image
+        val bitmap = BitmapFactory.decodeFile(imagePath)
+        holder.imageProfile?.setImageBitmap(bitmap)
+    }
+
+    override fun getItemCount() = images.size
+
+    fun setOnClickListener(listener: View.OnClickListener){
+        clickListener = listener
+    }
+
+    override fun onClick(view: View?) {
+        clickListener?.onClick(view)
+    }
+
+}
