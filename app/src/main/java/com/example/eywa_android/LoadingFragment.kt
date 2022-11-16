09a2223d6d9fb2381a845489b5cd47cb4a9ccf9b
Pau Pájaro Mondéjar <84421788+Paxsem@users.noreply.github.com>
2022-11-16 14:01:04 +0100

    Loading 75%

--- app/src/main/java/com/example/eywa_android/LoadingFragment.kt
+++ app/src/main/java/com/example/eywa_android/LoadingFragment.kt
@@ -6,8 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.activity.addCallback
+import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_loading.*
 
 
 class LoadingFragment : Fragment() {
@@ -17,6 +23,7 @@ class LoadingFragment : Fragment() {
         val callback = requireActivity().onBackPressedDispatcher.addCallback(this) {
 
         }
+
     }
 
     override fun onCreateView(
@@ -31,15 +38,44 @@ class LoadingFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+        txtPressAnywhereToContinue.isVisible = false
+        val animation_in : Animation = AnimationUtils.loadAnimation(this.context,R.anim.fade_in)
+        val animation_out : Animation = AnimationUtils.loadAnimation(this.context,R.anim.fade_out)
+        var pressed : Boolean = false
         val timer = object: CountDownTimer(5000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
 
             }
 
             override fun onFinish() {
-                findNavController().navigate(R.id.action_loadingFragment_to_loginFragment)
+                txtPressAnywhereToContinue.isVisible = true
+                txtPressAnywhereToContinue.startAnimation(animation_in)
+                while(!pressed)
+                {
+
+
+                val timer = object: CountDownTimer(3000, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+
+                    }
+
+                    override fun onFinish() {
+                        txtPressAnywhereToContinue.startAnimation(animation_out)
+                    }
+                }
+                timer.start()
+                }
+                TxtLoading.isVisible = false
+                loadingAnimation.isVisible = false
             }
         }
         timer.start()
+
+
+        loadingLayout.setOnClickListener(){
+            pressed = false
+            findNavController().navigate(R.id.action_loadingFragment_to_loginFragment)
+
+        }
     }
 }
