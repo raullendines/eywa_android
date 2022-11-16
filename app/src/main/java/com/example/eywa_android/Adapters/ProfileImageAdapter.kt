d97160bdda0444947601708b8513ea6b91fdad0c
MarcelHomar <99321235+MarcelHomar@users.noreply.github.com>
2022-11-16 12:11:07 +0100

    icon

--- app/src/main/java/com/example/eywa_android/Adapters/ProfileImageAdapter.kt
+++ app/src/main/java/com/example/eywa_android/Adapters/ProfileImageAdapter.kt
@@ -9,10 +9,73 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eywa_android.R
 
-class ProfileImageAdapter(private val context: Context,
-                          private val images: MutableList<String>) :
+class ProfileImageAdapter(private val context: Context) :
     RecyclerView.Adapter<ProfileImageAdapter.ImageViewHolder>(),
     View.OnClickListener{
+
+
+    private val images = mutableListOf<String>(
+        "antonio_banderas",
+        "joaquin_phoenix",
+        "spencer_tracy",
+        "clint_eastwood",
+        "robert_de_niro",
+        "leonardo_dicaprio",
+        "marlon_brando",
+        "emilia_clarkeCienc",
+        "bennedict_cumberbatch",
+        "russel_crowe",
+        "marty_mcfly",
+        "han_solo",
+        "robocop",
+        "luke_skywalker",
+        "tony_stark",
+        "katniss_everdeen",
+        "sam_bell",
+        "sarah_connor",
+        "frankenstein",
+        "neo",
+        "john_rambo",
+        "james_bond",
+        "indiana_jones",
+        "jack_sparrow",
+        "dr_evil",
+        "eggsy",
+        "viuda_negra",
+        "wonder_woman",
+        "hermoine_granger",
+        "bruce_lee",
+        "adam_sandler",
+        "borat",
+        "alig",
+        "noah_levenstein",
+        "bradley_cooper",
+        "melissa_mccarthy",
+        "jeff_lebowski",
+        "ryan_reynolds",
+        "jim_carrey",
+        "mclovin",
+        "po",
+        "shrek",
+        "minions",
+        "gato_botas",
+        "kowalski",
+        "alegria",
+        "simba",
+        "russell",
+        "woody",
+        "remy",
+        "jack_nicholson",
+        "pennywise",
+        "conde_dracula",
+        "hombre_lobo",
+        "imhotep",
+        "ghost_rider",
+        "blackheart",
+        "jinete_cabeza",
+        "ryan_macneil",
+        "anabelle"
+    )
 
     private val layout = R.layout.image_item
 
@@ -44,7 +107,7 @@ class ProfileImageAdapter(private val context: Context,
     fun bindImage(holder: ImageViewHolder, image: String){
         val imagePath = context.filesDir.toString() + "/img/" + image
         val bitmap = BitmapFactory.decodeFile(imagePath)
-        holder.imageProfile?.setImageBitmap(bitmap)
+        holder.imageProfile.setImageBitmap(bitmap)
     }
 
     override fun getItemCount() = images.size
