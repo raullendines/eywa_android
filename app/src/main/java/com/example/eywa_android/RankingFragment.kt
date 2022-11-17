a08e0ab054bd3dc291b90412af3dc3764622587d
Pau Pájaro Mondéjar <84421788+Paxsem@users.noreply.github.com>
2022-11-17 12:07:32 +0100

    Ranking començat

--- app/src/main/java/com/example/eywa_android/RankingFragment.kt
+++ app/src/main/java/com/example/eywa_android/RankingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_ranking.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,16 +18,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RankingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,26 +30,55 @@ class RankingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_ranking, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RankingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RankingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onStart() {
+        super.onStart()
+
+        val ranking = mutableListOf<UserRanking>(
+            UserRanking("Pau",15000,1, R.drawable.logo_eywa),
+            UserRanking("Pau",14000,2, R.drawable.logo_eywa),
+            UserRanking("Pau",13000,3, R.drawable.logo_eywa),
+            UserRanking("Pau",12000,4, R.drawable.logo_eywa),
+            UserRanking("Pau",11000,5, R.drawable.logo_eywa),
+            UserRanking("Pau",10000,6, R.drawable.logo_eywa),
+            UserRanking("Pau",9000,7, R.drawable.logo_eywa),
+            UserRanking("Pau",8000,8, R.drawable.logo_eywa),
+            UserRanking("Pau",7000,9, R.drawable.logo_eywa),
+            UserRanking("Pau",6000,10, R.drawable.logo_eywa),
+            UserRanking("Pau",5000,11, R.drawable.logo_eywa),
+            UserRanking("Pau",4000,12, R.drawable.logo_eywa),
+            UserRanking("Pau",3000,13, R.drawable.logo_eywa),
+            UserRanking("Pau",2000,14, R.drawable.logo_eywa),
+            UserRanking("Pau",1000,15, R.drawable.logo_eywa)
+        )
+
+        val top3 = mutableListOf<UserRanking>()
+
+        for (user : UserRanking in ranking){
+            if (user.rank == 1 || user.rank == 2 || user.rank == 3) {
+                top3.add(user)
             }
+        }
+
+        for (user : UserRanking in top3){
+            if (user.rank == 1){
+                imgRank1.setImageResource(user.image)
+                txtUsernameRank1.text = user.username
+                txtScoreRank1.text = user.score.toString()
+            }else if(user.rank == 2){
+                imgRank2.setImageResource(user.image)
+                txtUsernameRank2.text = user.username
+                txtScoreRank2.text = user.score.toString()
+            }else {
+                imgRank3.setImageResource(user.image)
+                txtUsernameRank3.text = user.username
+                txtScoreRank3.text = user.score.toString()
+            }
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/eywa_android/UserRanking.kt
@@ -0,0 +1,5 @@
+package com.example.eywa_android
+
+import android.media.Image
+
+class UserRanking(val username : String, val score : Int, val rank : Int, val image : Int)
