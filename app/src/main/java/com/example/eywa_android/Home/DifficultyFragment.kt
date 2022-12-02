63d7256b46186a77cbababb1d6448ff054346830
MarcelHomar <99321235+MarcelHomar@users.noreply.github.com>
2022-12-02 09:29:53 +0100

    Drama supported

--- app/src/main/java/com/example/eywa_android/Home/DifficultyFragment.kt
+++ app/src/main/java/com/example/eywa_android/Home/DifficultyFragment.kt
@@ -31,10 +31,10 @@ class DifficultyFragment : Fragment(), HomeActivity.mainPage {
 
     private var difficultySelected: Int = 0
 
-    private val sharedViewModel : HomeSharedViewModel by activityViewModels()
+    private val sharedViewModel: HomeSharedViewModel by activityViewModels()
 
 
-    private var _binding : FragmentDifficultyBinding? = null
+    private var _binding: FragmentDifficultyBinding? = null
     private val binding get() = _binding!!
 
 //    class ReturnUser : ActivityResultContract<User, Uri?>() {
@@ -55,15 +55,16 @@ class DifficultyFragment : Fragment(), HomeActivity.mainPage {
 //
 //    }
 
-    val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            result: ActivityResult ->
+    val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val intent = result.data
-                val newUser = intent!!.getSerializableExtra(QuestionsActivity.Questions.USER) as User
+                val newUser =
+                    intent!!.getSerializableExtra(QuestionsActivity.Questions.USER) as User
                 sharedViewModel.setUserToDisplay(newUser)
                 // Handle the Intent
             }
-    }
+        }
 
 
     override fun onCreateView(
@@ -90,25 +91,30 @@ class DifficultyFragment : Fragment(), HomeActivity.mainPage {
 
 
         sharedViewModel.difficulty.observe(viewLifecycleOwner) { difficulty ->
-            difficultyButtons[difficultySelected].backgroundTintList = requireContext().getColorStateList(
-                R.color.white
-            )
+            difficultyButtons[difficultySelected].backgroundTintList =
+                requireContext().getColorStateList(
+                    R.color.white
+                )
             when (difficulty) {
                 0 -> {
                     difficultySelected = 0
-                    difficultyButtons[0].backgroundTintList = requireContext().getColorStateList(R.color.green)
+                    difficultyButtons[0].backgroundTintList =
+                        requireContext().getColorStateList(R.color.green)
                 }
                 1 -> {
                     difficultySelected = 1
-                    difficultyButtons[1].backgroundTintList = requireContext().getColorStateList(R.color.orange)
+                    difficultyButtons[1].backgroundTintList =
+                        requireContext().getColorStateList(R.color.orange)
                 }
                 2 -> {
                     difficultySelected = 2
-                    difficultyButtons[2].backgroundTintList = requireContext().getColorStateList(R.color.red)
+                    difficultyButtons[2].backgroundTintList =
+                        requireContext().getColorStateList(R.color.red)
                 }
                 3 -> {
                     difficultySelected = 3
-                    difficultyButtons[3].backgroundTintList = requireContext().getColorStateList(R.color.amarillo_eywa)
+                    difficultyButtons[3].backgroundTintList =
+                        requireContext().getColorStateList(R.color.amarillo_eywa)
                 }
 
             }
@@ -118,80 +124,79 @@ class DifficultyFragment : Fragment(), HomeActivity.mainPage {
         //category = arguments?.getString(QuestionsActivity.Questions.CATEGORY)
         setCategoryColor(sharedViewModel.category.value.toString())
 
-        for(index in 0..3){
-            difficultyButtons[index].setOnClickListener(){
+        for (index in 0..3) {
+            difficultyButtons[index].setOnClickListener() {
                 sharedViewModel.changeDifficulty(index)
             }
         }
 
-        binding.btnPlay.setOnClickListener(){
-
-            if(sharedViewModel.category.value.toString() != "Drama"){
-                val intentQuestion = Intent(this.activity, QuestionsActivity::class.java)
-
-                var Questions : MutableList<Question> = mutableListOf()
-
-                var locale : Locale? = null
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-                    locale = resources.configuration.locales.get(0);
-                } else{
-                    //noinspection deprecation
-                    locale = resources.configuration.locale
-                }
-                val localeLang = locale!!.language
+        binding.btnPlay.setOnClickListener() {
 
 
+            val intentQuestion = Intent(this.activity, QuestionsActivity::class.java)
 
-                when(localeLang){
-                    "ca" -> Questions = FilesManager.getQuestionsCA(requireContext())
-                    "es" -> Questions = FilesManager.getQuestionsES(requireContext())
-                    "en" -> Questions = FilesManager.getQuestionsEN(requireContext())
-                }
+            var Questions: MutableList<Question> = mutableListOf()
 
-                //We have to add 1 to the difficulty because Easy = 1 and for the array its easier to start for 0
-                val d : Int = sharedViewModel.difficulty.value as Int
-                val difficulty : String = (d + 1).toString()
-
-                val category = sharedViewModel.category.value.toString().lowercase()
-
-                var questionsReturn : MutableList<Question> = mutableListOf<Question>()
-                for(q in Questions){
-                    if(q.category == category && q.difficulty == difficulty){
-                        questionsReturn.add(q)
-                    }
-                }
-
-                if(questionsReturn.size > 10){
-                    questionsReturn.shuffle()
-                    questionsReturn = questionsReturn.drop(questionsReturn.size - 10).toMutableList()
-                    //questionsReturn.subList(10, Questions.size).clear()
-                }
-
-                if (questionsReturn.size != 10){
-
-                    val noQ = Question("100000", "No hay suficientes preguntas",
-                        category, difficulty,
-                        "This is the correct one",
-                        arrayOf("Not this one", "Not this one", "Not this one")
-                    )
-
-                    for (i in questionsReturn.size..10){
-                        questionsReturn.add(noQ)
-                    }
-                }
-
-                val myActivity = this.activity as HomeActivity
-
-
-                intentQuestion.putParcelableArrayListExtra(QuestionsActivity.Questions.QUESTIONS, questionsReturn as ArrayList<Question>)
-                intentQuestion.putExtra(QuestionsActivity.Questions.USER, sharedViewModel.displayUser!!)
-
-                startForResult.launch(intentQuestion)
-
-                //startActivity(intentQuestion)
-            } else{
-                Toast.makeText(requireContext(), "Drama questions not supported", Toast.LENGTH_LONG).show()
+            var locale: Locale? = null
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                locale = resources.configuration.locales.get(0);
+            } else {
+                //noinspection deprecation
+                locale = resources.configuration.locale
             }
+            val localeLang = locale!!.language
+
+
+
+            when (localeLang) {
+                "ca" -> Questions = FilesManager.getQuestionsCA(requireContext())
+                "es" -> Questions = FilesManager.getQuestionsES(requireContext())
+                "en" -> Questions = FilesManager.getQuestionsEN(requireContext())
+            }
+
+            //We have to add 1 to the difficulty because Easy = 1 and for the array its easier to start for 0
+            val d: Int = sharedViewModel.difficulty.value as Int
+            val difficulty: String = (d + 1).toString()
+
+            val category = sharedViewModel.category.value.toString().lowercase()
+
+            var questionsReturn: MutableList<Question> = mutableListOf<Question>()
+            for (q in Questions) {
+                if (q.category == category && q.difficulty == difficulty) {
+                    questionsReturn.add(q)
+                }
+            }
+
+            if (questionsReturn.size > 10) {
+                questionsReturn.shuffle()
+                questionsReturn = questionsReturn.drop(questionsReturn.size - 10).toMutableList()
+                //questionsReturn.subList(10, Questions.size).clear()
+            }
+
+            if (questionsReturn.size != 10) {
+
+                val noQ = Question(
+                    "100000", "No hay suficientes preguntas",
+                    category, difficulty,
+                    "This is the correct one",
+                    arrayOf("Not this one", "Not this one", "Not this one")
+                )
+
+                for (i in questionsReturn.size..10) {
+                    questionsReturn.add(noQ)
+                }
+            }
+
+            val myActivity = this.activity as HomeActivity
+
+
+            intentQuestion.putParcelableArrayListExtra(
+                QuestionsActivity.Questions.QUESTIONS,
+                questionsReturn as ArrayList<Question>
+            )
+            intentQuestion.putExtra(QuestionsActivity.Questions.USER, sharedViewModel.displayUser!!)
+
+            startForResult.launch(intentQuestion)
 
         }
     }
@@ -216,8 +221,8 @@ class DifficultyFragment : Fragment(), HomeActivity.mainPage {
 
     }
 
-    private fun setCategoryColor(category: String){
-        when (category){
+    private fun setCategoryColor(category: String) {
+        when (category) {
             "Action" -> {
                 binding.buttonCategory.backgroundTintList =
                     requireContext().getColorStateList(R.color.action)
@@ -257,12 +262,10 @@ class DifficultyFragment : Fragment(), HomeActivity.mainPage {
         }
         binding.textCategory.setTextColor(Color.parseColor("#FFFFFF"))
 
-        binding.buttonCategory.setOnClickListener(){
+        binding.buttonCategory.setOnClickListener() {
             findNavController().popBackStack()
         }
     }
-
-
 
 
 }
