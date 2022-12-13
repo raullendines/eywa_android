f3c21534282652192209de244526b8e1a7219d39
Raul Lendines <91599628+raullendines@users.noreply.github.com>
2022-12-13 13:38:32 +0100

    Animation done

--- app/src/main/java/com/example/eywa_android/Loading_Login/LoginFragment.kt
+++ app/src/main/java/com/example/eywa_android/Loading_Login/LoginFragment.kt
@@ -1,16 +1,21 @@
 package com.example.eywa_android.Loading_Login
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
 import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.*
 import androidx.activity.addCallback
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
+import com.airbnb.lottie.LottieAnimationView
 import com.example.eywa_android.Home.HomeActivity
 import com.example.eywa_android.R
 import com.example.eywa_android.ClassObject.User
@@ -18,6 +23,7 @@ import com.example.eywa_android.Utility.Bcrypt
 import com.example.eywa_android.Utility.FilesManager
 import com.example.eywa_android.databinding.FragmentDifficultyBinding
 import com.example.eywa_android.databinding.FragmentLoginBinding
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
@@ -87,9 +93,33 @@ class LoginFragment : Fragment() {
 
     }
 
-    fun loginFun(users : MutableList<User>){
+    fun animationLogin(animation: LottieAnimationView){
+        animation.setAnimation(R.raw.loading_login)
+        animation.visibility = View.VISIBLE
+        animation.alpha = 1.0f
+        animation.playAnimation()
 
+        var layoutLogin = binding.layoutLogin;
+        layoutLogin.setBackgroundColor(Color.parseColor("#A6000000"));
+
+        object : CountDownTimer(4000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                Handler().postDelayed({
+                    layoutLogin.setBackgroundColor(Color.parseColor("#00000000"));
+                    animation.visibility = View.INVISIBLE
+                }, 100)
+            }
+        }.start()
+    }
+
+    fun loginFun(users : MutableList<User>){
         var userExists = userExist(binding.textUsername.text.toString(),binding.textPassword.text.toString(),users)
+
+        animationLogin(binding.animationLogin)
 
         //Omplir amb les dades per contrastar amb el JSON
         if (binding.textUsername.text.isEmpty() || binding.textPassword.text.isEmpty()){
